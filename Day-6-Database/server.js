d2ac3425c4259d6954d6a0bfef0ad71739a7a2d4
Manish kumar <memanish2525@gmail.com>
2026-02-04 21:24:30 +0530

    Day-6-Database

---
+++ Day-6-Database/server.js
@@ -0,0 +1,15 @@
+
+const app=require('./src/app');
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
---
+++ Day-6-Database/src/app.js
@@ -0,0 +1,38 @@
+const express= require('express');
+
+const app=express();
+
+app.use(express.json())
+
+const notes=[]
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+    res.status(201).json({
+        message:"Note created successfully"
+    })
+})
+app.get('/notes',(req,res)=>{
+    res.status(200).json({
+        notes:notes
+    })
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index];
+    res.status(200).json({
+        message:"Note deleted successfully"
+
+    })
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    notes[req.params.index].Description=req.body.Description;
+    res.status(200).json({
+        message:"Note updated successfully"
+    })
+})
+
+
+module.exports=app;
